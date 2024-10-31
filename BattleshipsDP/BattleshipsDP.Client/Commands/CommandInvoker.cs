fa28a9922338fdc606bd3da3da22f6cb163d4a7c
Vitas Ilekis <vitile@ktu.lt>
2024-10-31 09:07:06 +0200

    Added command pattern to shoot history

---
+++ BattleshipsDP/BattleshipsDP.Client/Commands/CommandInvoker.cs
@@ -0,0 +1,44 @@
+﻿using SharedLibrary;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BattleshipsDP.Client
+{
+    public class CommandInvoker
+    {
+        private readonly List<ShotCommand> _commandHistory = new();
+
+        public void ExecuteCommand(ShotCommand command, List<List<GridCell>> board)
+        {
+            var existingCommand = _commandHistory.FirstOrDefault(
+                c => c.Row == command.Row && c.Col == command.Col && c.IsPlayerShot != command.IsPlayerShot
+            );
+
+            if (existingCommand == null)
+            {
+                command.Execute(board);
+                _commandHistory.Add(command);
+            }
+            else
+            {
+                _commandHistory.Remove(existingCommand);
+                var updatedCommand = new ShotCommand(existingCommand.Row, existingCommand.Col, command.ShotType, existingCommand.IsPlayerShot);
+                updatedCommand.Execute(board);
+                _commandHistory.Add(updatedCommand);
+            }
+        }
+
+
+        //public void UndoLastCommand(List<List<GridCell>> board)
+        //{
+        //    if (_commandHistory.Count > 0)
+        //    {
+        //        var lastCommand = _commandHistory.Last();
+        //        lastCommand.Undo(board);
+        //        _commandHistory.Remove(lastCommand);
+        //    }
+        //}
+
+        public IEnumerable<ShotCommand> GetHistory() => _commandHistory;
+    }
+}
---
+++ SharedLibrary/ShotCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary
+{
+    public class ShotCommand
+    {
+        public int Row { get; }
+        public int Col { get; }
+        public string ShotType { get; }
+        public bool IsPlayerShot { get; }
+
+        public ShotCommand(int row, int col, string shotType, bool isPlayerShot)
+        {
+            Row = row;
+            Col = col;
+            ShotType = shotType;
+            IsPlayerShot = isPlayerShot;
+        }
+
+        public void Execute(List<List<GridCell>> board)
+        {
+            var cell = board[Row][Col];
+            cell.IsHighlighted = true;
+            cell.State = ShotType == "hit" ? Square.Hit : Square.Miss;  // Assume basic hit/miss outcome for simplicity
+        }
+
+        public void Undo(List<List<GridCell>> board)
+        {
+            var cell = board[Row][Col];
+            cell.IsHighlighted = false;
+            cell.State = Square.Empty;  // Reset to original state
+        }
+    }
+}
