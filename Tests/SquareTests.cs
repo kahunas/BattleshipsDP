ec109876218117c9ebd61456d55109dfbf9aa811
Whyytas <vitas.ilekis@ktu.edu>
2024-12-05 23:08:00 +0200

    Added tests

---
+++ Tests/SquareTests.cs
@@ -0,0 +1,25 @@
+﻿// File: SharedLibrary.Tests/EnumExtensionsTests.cs
+using System;
+using Xunit;
+using SharedLibrary;
+using System.ComponentModel;
+
+namespace Tests
+{
+    public class SquareTests
+    {
+        [Theory]
+        [InlineData(Square.Empty, " ")]
+        [InlineData(Square.Ship, "S")]
+        [InlineData(Square.Hit, "X")]
+        [InlineData(Square.Miss, "M")]
+        public void GetDescription_ReturnsCorrectDescription_ForEnumWithDescription(Square square, string expectedDescription)
+        {
+            // Act
+            var description = square.GetDescription();
+
+            // Assert
+            Assert.Equal(expectedDescription, description);
+        }
+    }
+}
---
+++ Tests/TeamTests.cs
@@ -0,0 +1,107 @@
+﻿// File: SharedLibrary.Tests/TeamTests.cs
+using System;
+using System.Collections.Generic;
+using Xunit;
+using SharedLibrary;
+using SharedLibrary.Factory;
+using SharedLibrary.Bridge;
+
+namespace Tests
+{
+    public class TeamTests
+    {
+        private class TestShot
+        {
+            public int Amount { get; set; }
+            public TestShot(int amount)
+            {
+                Amount = amount;
+            }
+        }
+
+        [Fact]
+        public void Constructor_InitializesPropertiesCorrectly()
+        {
+            // Arrange
+            string teamName = "Team A";
+
+            // Act
+            var team = new Team(teamName);
+
+            // Assert
+            Assert.Equal(teamName, team.Name);
+            Assert.NotNull(team.Board);
+            Assert.NotNull(team.Players);
+            Assert.NotEmpty(team.ShotCollection);
+        }
+
+        [Fact]
+        public void TakeShot_DecreasesShotAmount_WhenShotTypeExistsAndHasShots()
+        {
+            // Arrange
+            var team = new Team("Team A");
+            var initialAmount = team.ShotCollection[0].Amount;
+
+            // Act
+            var result = team.TakeShot(typeof(SimpleShot));
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(initialAmount - 1, team.ShotCollection[0].Amount);
+        }
+
+        [Fact]
+        public void TakeShot_ReturnsFalse_WhenShotTypeDoesNotExist()
+        {
+            // Arrange
+            var team = new Team("Team A");
+
+            // Act
+            var result = team.TakeShot(typeof(TestShot));
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TakeShot_ReturnsFalse_WhenShotTypeHasNoShots()
+        {
+            // Arrange
+            var team = new Team("Team A");
+            team.ShotCollection[0].Amount = 0;
+
+            // Act
+            var result = team.TakeShot(typeof(SimpleShot));
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void RemainingShots_ReturnsCorrectAmount_WhenShotTypeExists()
+        {
+            // Arrange
+            var team = new Team("Team A");
+            var expectedAmount = team.ShotCollection[0].Amount;
+
+            // Act
+            var result = team.RemainingShots(typeof(SimpleShot));
+
+            // Assert
+            Assert.Equal(expectedAmount, result);
+        }
+
+        [Fact]
+        public void RemainingShots_ReturnsNegativeOne_WhenShotTypeDoesNotExist()
+        {
+            // Arrange
+            var team = new Team("Team A");
+
+            // Act
+            var result = team.RemainingShots(typeof(TestShot));
+
+            // Assert
+            Assert.Equal(-1, result);
+        }
+    }
+}
